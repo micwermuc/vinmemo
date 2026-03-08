813632155a3ccffffe7b960bb03b36e4cb36c28c
micwermuc <werner.michael@hotmail.com>
2026-03-08 19:19:25 +0100

    Add files via upload
    v0.91 bug fixes

--- app.js
+++ app.js
@@ -20,13 +20,12 @@ card.className="wineCard";
 
 card.innerHTML=`
 <b>${wine.name}</b><br>
-${wine.producer}<br>
-${wine.vintage} • ${wine.region}<br>
+${wine.producer||""}<br>
+${wine.vintage||""} • ${wine.region||""}<br>
 🍷 ${wine.remaining} | getrunken ${wine.drank}
 `;
 
 card.onclick=()=>openDetail(i);
-
 return card;
 }
 
@@ -42,26 +41,29 @@ if(archiv)archiv.innerHTML="";
 
 wines.forEach((wine,i)=>{
 
-if(wine.remaining>0){
+if(wine.remaining>0 && keller){
 keller.appendChild(renderCard(wine,i));
 }
 
-if(wine.wishlist){
+if(wine.wishlist && wishlist){
 wishlist.appendChild(renderCard(wine,i));
 }
 
-if(wine.remaining===0){
+if(wine.remaining===0 && archiv){
 archiv.appendChild(renderCard(wine,i));
 }
 
 });
 
+if(document.getElementById("countKeller"))
 document.getElementById("countKeller").innerText =
 wines.reduce((a,w)=>a+w.remaining,0)+" Flaschen";
 
+if(document.getElementById("countWishlist"))
 document.getElementById("countWishlist").innerText =
 wines.filter(w=>w.wishlist).length+" Weine";
 
+if(document.getElementById("countArchiv"))
 document.getElementById("countArchiv").innerText =
 wines.filter(w=>w.remaining===0).length+" Weine";
 
@@ -73,18 +75,18 @@ currentWineIndex=i;
 let wine=wines[i];
 
 document.getElementById("viewName").innerText=wine.name;
-document.getElementById("viewProducer").innerText=wine.producer;
-document.getElementById("viewVintage").innerText=wine.vintage;
-document.getElementById("viewRegion").innerText=wine.region;
+document.getElementById("viewProducer").innerText=wine.producer||"";
+document.getElementById("viewVintage").innerText=wine.vintage||"";
+document.getElementById("viewRegion").innerText=wine.region||"";
 document.getElementById("viewCounter").innerText="Verbleibend "+wine.remaining+" | Getrunken "+wine.drank;
 document.getElementById("viewNotes").innerText=wine.notes||"";
 
 document.getElementById("editName").value=wine.name;
-document.getElementById("editProducer").value=wine.producer;
-document.getElementById("editVintage").value=wine.vintage;
-document.getElementById("editRegion").value=wine.region;
+document.getElementById("editProducer").value=wine.producer||"";
+document.getElementById("editVintage").value=wine.vintage||"";
+document.getElementById("editRegion").value=wine.region||"";
 document.getElementById("editNotes").value=wine.notes||"";
-document.getElementById("editWishlist").checked=wine.wishlist;
+document.getElementById("editWishlist").checked=wine.wishlist||false;
 
 editMode=false;
 document.getElementById("editMode").classList.add("hidden");
@@ -104,9 +106,9 @@ document.getElementById("viewMode").classList.toggle("hidden");
 
 function closeModal(){
 
-if(editMode){
-
 let wine=wines[currentWineIndex];
+
+if(editMode){
 
 wine.name=document.getElementById("editName").value;
 wine.producer=document.getElementById("editProducer").value;
@@ -123,12 +125,12 @@ render();
 
 }
 
-window.onclick=function(e){
+window.addEventListener("click",function(e){
 let modal=document.getElementById("detailModal");
 if(e.target===modal){
 closeModal();
 }
-}
+});
 
 function drinkBottle(){
 
@@ -149,7 +151,6 @@ function addPurchase(){
 let wine=wines[currentWineIndex];
 
 let amount=Number(prompt("Wie viele Flaschen gekauft?"));
-
 if(!amount)return;
 
 wine.remaining+=amount;
@@ -185,7 +186,7 @@ name,
 producer,
 vintage,
 region,
-remaining:amount,
+remaining:amount||0,
 drank:0,
 wishlist:false,
 notes:""
